7411994281ef2041d297dc0667e589b68f62ebe9
Bruno Paulino <brunojppb@gmail.com>
2016-06-14 20:31:04 -0300

    thinking about recursion on Map/Reduce

--- list_and_recursion/my_list.exs
+++ list_and_recursion/my_list.exs
@@ -12,4 +12,13 @@ defmodule MyList do
   def map([], func), do: []
   def map([ head | tail ], func), do: [ func.(head) | map(tail, func) ]
 
+  # show a public function that will handle the array and call
+  # a private one to do the job with the extra arguments that
+  # the user doesn't need to care about
+  def sum(list), do: _sum(list, 0)
+  # This way, we can keep the 0 away and hide the real logic in
+  # private functions
+  defp _sum([], total), do: total
+  defp _sum([ head | tail ], total), do: _sum(tail, head+total)
+
 end
