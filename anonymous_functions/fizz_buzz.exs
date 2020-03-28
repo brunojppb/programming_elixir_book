5597fe7dc99d97f3613037e6cd7aa754a745bc90
Bruno Paulino <bruno@woombikes.com>
2020-03-28 16:55:01 +0100

    use rem func

--- anonymous_functions/fizz_buzz.exs
+++ anonymous_functions/fizz_buzz.exs
@@ -9,3 +9,13 @@ IO.puts(fizz_buzz.(0, 0, 3))
 IO.puts(fizz_buzz.(1, 0, 3))
 IO.puts(fizz_buzz.(0, 1, 3))
 IO.puts(fizz_buzz.(1, 2, 3))
+
+fizz_buzz_rem = fn n -> fizz_buzz.(rem(n, 3), rem(n, 5), n) end
+IO.puts("Using rem:")
+IO.puts(fizz_buzz_rem.(10))
+IO.puts(fizz_buzz_rem.(11))
+IO.puts(fizz_buzz_rem.(12))
+IO.puts(fizz_buzz_rem.(13))
+IO.puts(fizz_buzz_rem.(14))
+IO.puts(fizz_buzz_rem.(15))
+IO.puts(fizz_buzz_rem.(16))
